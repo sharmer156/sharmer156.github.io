58fe2ba156300dc8e7cf772e8450768d64da3cad
sharmer156 <33123492+sharmer156@users.noreply.github.com>
2018-12-03 11:05:50 +0800

    update

--- test/upload/js/index_0bak.js
+++ test/upload/js/index_0bak.js
@@ -10,9 +10,9 @@ const uppy = Uppy({
   debug: true,
   autoProceed: false,
   restrictions: {
-    maxFileSize: 5000000,
-    maxNumberOfFiles: 10,
-    minNumberOfFiles: 1,
+    maxFileSize: 1000000,
+    maxNumberOfFiles: 3,
+    minNumberOfFiles: 2,
     allowedFileTypes: ['image/*', 'video/*']
   }
 })
@@ -22,7 +22,7 @@ const uppy = Uppy({
   target: '.DashboardContainer',
   replaceTargetContent: true,
   showProgressDetails: true,
-  note: 'Images and video only, 1–10 files, up to 5 MB',
+  note: 'Images and video only, 2–3 files, up to 1 MB',
   height: 470,
   metaFields: [
     { id: 'name', name: 'Name', placeholder: 'file name' },
@@ -30,11 +30,11 @@ const uppy = Uppy({
   ],
   browserBackButtonClose: true
 })
-.use(GoogleDrive, { target: Dashboard, serverUrl: 'https://sharmer156.github.io/test/upload/index' })
-.use(Dropbox, { target: Dashboard, serverUrl: 'https://sharmer156.github.io/test/upload/index' })
-.use(Instagram, { target: Dashboard, serverUrl: 'https://sharmer156.github.io/test/upload/index' })
+.use(GoogleDrive, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+.use(Dropbox, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+.use(Instagram, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
 .use(Webcam, { target: Dashboard })
-.use(Tus, { endpoint: 'https://sharmer156.github.io/test/upload/index/upfiles' })
+.use(Tus, { endpoint: 'https://master.tus.io/files/' })
 
 uppy.on('complete', result => {
   console.log('successful files:', result.successful)
